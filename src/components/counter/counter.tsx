ce9d4da068d51e9b2ef068a8f599aae0e26997ab
Madeleine Olson <madeleineolson@outlook.com>
2021-01-29 11:30:28 +0100

    updates

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { increment, decrement } from '../../reducers/counterReducer';
 import './counter.scss';
--- src/components/showCount/showCount.tsx
+++ src/components/showCount/showCount.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../reducers/rootReducer';
 import './showCount.scss';
